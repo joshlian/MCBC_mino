4857325ee2a37ed1154c49841ba8fbbb5943ea28
joshlian <jtl5655@mavs.uta.edu>
2024-05-23 09:32:51 -0500

    changed dashboard name

---
+++ frontend/src/pages/LandingPage.js
@@ -0,0 +1,7 @@
+const LandingPage = () => {
+    return (
+        <h1>first page</h1>
+    );
+}
+
+export default LandingPage;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+import LandingPage from './pages/LandingPage';
 import MinoInfo from './pages/MinoInfo';
 
 export const PageRoutes = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Dashboard />} />
+                <Route path="/" element={<LandingPage />} />
                 <Route path="/MinoInfo" element={<MinoInfo />} />
             </Routes>
         </Router>
