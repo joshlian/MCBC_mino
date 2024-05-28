8c26f72cea090d45c8cd227b7250c0ec662b5561
joshlian <jtl5655@mavs.uta.edu>
2024-05-28 10:49:13 -0500

    added a css file

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,45 +1,21 @@
-import React, { Component } from "react";
+import React from 'react';
+import './LandingPage.css';
 
-class LandingPage extends Component {
-    redirectToMinoInfoPage = () => {
-        window.location.href = "/MinoInfo";
-    };
-
-    render() {
-        const myStyle = {
-            backgroundImage: "url(https://wallpapers.com/images/high/plain-light-blue-background-3440-x-1440-pyax2q0t5zzmmjsn.webp)",
-            height: "100vh",
-            marginTop: "-70px",
-            fontSize: "50px",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-            color: "#ffffff",
-        };
-
-        const buttonStyle = {
-            padding: "8px 15px",
-            fontSize: "20px",
-            backgroundColor: "#ffffff",
-            color: "#000000",
-            border: "none",
-            cursor: "pointer",
-        };
-
-        return (
-            <div style={myStyle}>
-                <div>
-                    <h1>Chruch Info</h1>
-                    <button style={buttonStyle} onClick={this.redirectToMinoInfoPage}>Mino Info</button>
-                </div>
-            </div>
-        );
-    }
+const MinoinfoPage = () => {
+    window.location.href = "/MinoInfo";
+};
+const LandingPage = () => {
+    return (
+        <div className="LandingPage"> 
+            <header className="LandingPage-header"> 
+                <div className="info-box">Midland Chin Baptist Church</div>
+                <button className="custom-button" onClick={MinoinfoPage}>Mino Info</button>
+            </header>
+            {/* Other content of your landing page */}
+        </div>
+    );
 }
 
 export default LandingPage;
+
 
